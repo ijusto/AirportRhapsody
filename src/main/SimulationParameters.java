b4239e2f16422a7881b2b4d1b6df86326f04ff84
Inês Justo <inesjusto@ua.pt>
2020-03-29 00:54:46 +0000

    simulpar corrected

--- src/main/SimulationParameters.java
+++ src/main/SimulationParameters.java
@@ -3,15 +3,15 @@ package main;
 public class SimulationParameters {
 
     /* number of passengers per flight */
-    public static final int N_PASS_PER_FLIGHT = 6; /* TODO: Change value*/
+    public static final int N_PASS_PER_FLIGHT = 6;
 
     /* number of flights/landings */
-    public static final  int N_FLIGHTS = 5; /* TODO: Change value*/
+    public static final  int N_FLIGHTS = 5;
 
     /* number of pieces of baggage per passenger */
-    public static final  int N_BAGS_PER_PASS = 4; /* TODO: Change value*/
+    public static final  int N_BAGS_PER_PASS = 2;
 
     /* bus capacity */
-    public static final  int BUS_CAP = 5; /* TODO: Change value*/
+    public static final  int BUS_CAP = 3;
 
 }
