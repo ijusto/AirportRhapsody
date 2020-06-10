cfda90e55b163ab89f777813a6cbac2941b0d445
Miguel Lopes <m.lopes@ua.pt>
2020-06-10 18:27:03 +0100

    compile shell

--- DistributedVersion/build_and_deploy.sh
+++ DistributedVersion/build_and_deploy.sh
@@ -28,24 +28,61 @@ javac serverSide/proxies/TemporaryStorageAreaProxy.java
 
 #Servers
 
-javac serverSide/Servers/ServerArrivalLounge.java
-javac serverSide/proxies/ServerArrivalTerminalExit.java
-javac serverSide/proxies/ServerArrivalTermTransfQuay.java
-javac serverSide/proxies/ServerBaggageColPoint.java
-javac serverSide/proxies/ServerBaggageReclaimOffice.java
-javac serverSide/proxies/ServerDepartureTerminalEntrance.java
-javac serverSide/proxies/ServerDepartureTermTransfQuay.java
-javac serverSide/proxies/ServerGenReposInfo.java
-javac serverSide/proxies/ServerTemporaryStorageArea.java
+javac serverSide/servers/ServerArrivalLounge.java
+javac serverSide/servers/ServerArrivalTerminalExit.java
+javac serverSide/servers/ServerArrivalTermTransfQuay.java
+javac serverSide/servers/ServerBaggageColPoint.java
+javac serverSide/servers/ServerBaggageReclaimOffice.java
+javac serverSide/servers/ServerDepartureTerminalEntrance.java
+javac serverSide/servers/ServerDepartureTermTransfQuay.java
+javac serverSide/servers/ServerGenReposInfo.java
+javac serverSide/servers/ServerTemporaryStorageArea.java
 
 #Shared Regions
 
-javac serverSide/Servers/ArrivalLounge.java
-javac serverSide/proxies/ArrivalTerminalExit.java
-javac serverSide/proxies/ArrivalTermTransfQuay.java
-javac serverSide/proxies/BaggageColPoint.java
-javac serverSide/proxies/BaggageReclaimOffice.java
-javac serverSide/proxies/DepartureTerminalEntrance.java
-javac serverSide/proxies/DepartureTermTransfQuay.java
-javac serverSide/proxies/GenReposInfo.java
-javac serverSide/proxies/TemporaryStorageArea.java
+javac serverSide/sharedRegions/ArrivalLounge.java
+javac serverSide/sharedRegions/ArrivalTerminalExit.java
+javac serverSide/sharedRegions/ArrivalTermTransfQuay.java
+javac serverSide/sharedRegions/BaggageColPoint.java
+javac serverSide/sharedRegions/BaggageReclaimOffice.java
+javac serverSide/sharedRegions/DepartureTerminalEntrance.java
+javac serverSide/sharedRegions/DepartureTermTransfQuay.java
+javac serverSide/sharedRegions/GenReposInfo.java
+javac serverSide/sharedRegions/TemporaryStorageArea.java
+
+#ServerCom
+
+javac serverSide/ServerCom.java
+
+echo "Compiling Client Side"
+
+#Clients
+
+javac clientSide/clients/ClientBusDriver.java
+javac clientSide/clients/ClientPassenger.java
+javac clientSide/clients/ClientPorter.java
+
+#Entities
+
+javac clientSide/entities/BusDriver.java
+javac clientSide/entities/BusDriverStates.java
+javac clientSide/entities/Porter.java
+javac clientSide/entities/PorterStates.java
+javac clientSide/entities/Passenger.java
+javac clientSide/entities/PassengerStates.java
+
+
+#SharedRegionStubs
+
+javac clientSide/sharedRegionsStubs/ArrivalLoungeStub.java
+javac clientSide/sharedRegionsStubs/ArrivalTerminalExitStub.java
+javac clientSide/sharedRegionsStubs/ArrivalTermTransfQuayStub.java
+javac clientSide/sharedRegionsStubs/BaggageColPointStub.java
+javac clientSide/sharedRegionsStubs/BaggageReclaimOfficeStub.java
+javac clientSide/sharedRegionsStubs/DepartureTerminalEntranceStub.java
+javac clientSide/sharedRegionsStubs/DepartureTermTransfQuayStub.java
+javac clientSide/sharedRegionsStubs/GenReposInfoStub.java
+javac clientSide/sharedRegionsStubs/TemporaryStorageAreaStub.java
+
+#ClientCom
+javac clientSide/ClientCom.java
