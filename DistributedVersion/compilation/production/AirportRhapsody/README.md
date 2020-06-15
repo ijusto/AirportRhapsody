b85bba8b54c075c967eece6c39b40dfe66e1006a
Inês Justo <inesjusto@ua.pt>
2020-06-15 01:04:33 +0100

    deleted generated directory

--- DistributedVersion/compilation/production/AirportRhapsody/README.md
+++
@@ -1,2 +0,0 @@
-# AirportRhapsody
-Distributed Version
--- DistributedVersion/compilation/production/AirportRhapsody/run_clients.sh
+++
@@ -1,4 +0,0 @@
-#Clients
-gnome-terminal -x bash -c "java clientSide/clients/ClientPassenger | tee ClientPassenger.txt"
-gnome-terminal -x bash -c "java clientSide/clients/ClientPorter | tee ClientPorter.txt"
-gnome-terminal -x bash -c "java clientSide/clients/ClientBusDriver | tee ClientBusDriver.txt"
